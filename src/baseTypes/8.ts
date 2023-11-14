5869f4863285f38b820a2bcff6194988cd41f03a
KarinaPavliuk <kpavliuk8@gmail.com>
2023-11-15 00:12:14 +0200

    Update 8.ts

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -3,6 +3,9 @@
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
 
-const myGender;
+type Genres = "male" | "female";
+
+
+const myGender: Genres = "female";
 
 export {};
